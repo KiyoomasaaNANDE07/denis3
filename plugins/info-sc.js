823b45cfa93966e4d59b3c9b6c015e6d42384b4a
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-06-10 22:02:08 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -12,7 +12,7 @@ let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
 Nih Link Sc Nya Dibawah :
-https://youtu.be/AXPzjQ5jxx8
+wa.me/6288217843188
 
 _*Jangan Lupa Di Subscribe*_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
